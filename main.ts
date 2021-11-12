5f5c18feca8b6b45abf04d82a95070f106791908
moofMonkey <1892802-moofMonkey@users.noreply.gitlab.com>
2021-11-12 07:39:55 +0300

    Initial commit

---
+++ main.ts
@@ -0,0 +1,7 @@
+import { RendererSDK } from "./wrapper/Imports"
+
+RendererSDK.CreateFont("Noto Sans Traditional Chinese", "fonts/NotoSansTC/NotoSansTC-Bold.otf", 700, false)
+RendererSDK.CreateFont("Noto Sans Traditional Chinese", "fonts/NotoSansTC/NotoSansTC-Regular.otf", 400, false)
+
+RendererSDK.CreateFont("Noto Sans Simplified Chinese", "fonts/NotoSansSC/NotoSansSC-Bold.otf", 700, false)
+RendererSDK.CreateFont("Noto Sans Simplified Chinese", "fonts/NotoSansSC/NotoSansSC-Regular.otf", 400, false)
