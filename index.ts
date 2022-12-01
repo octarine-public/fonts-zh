98249374315a87677581e33cc94b75b5c3a5b242
SicdeX <sicdex@gmail.com>
2022-12-01 12:56:20 +0600

    lint

--- index.ts
+++ index.ts
@@ -2,10 +2,11 @@ import { tryFindFile } from "github.com/octarine-public/wrapper/index"
 
 function LoadFontWrapper(path: string, isFallback = false) {
 	const realPath = tryFindFile(path)
-	if (realPath !== undefined)
+	if (realPath !== undefined) {
 		LoadFont(realPath, isFallback)
-	else
+	} else {
 		console.error(`Failed to find font file ${path}`)
+	}
 }
 
 LoadFontWrapper("fonts/NotoSansTC/NotoSansTC-Bold.otf")
